822b7d57a61742970c526e8973757b61f540e695
Terry Crowley <tcrowley@alum.mit.edu>
2024-12-30 14:39:56 -0800

    Make all multiblockmapping apis work for a multi part mapping.

--- lib/geo/multiblockmapping.ts
+++ lib/geo/multiblockmapping.ts
@@ -84,7 +84,7 @@ export class MultiBlockMapping
 
   rev(geoid: string): string[]
   {
-    for (let i = 0; i < this.entries.length; i++)
+    for (let i = this.entries.length-1; i >= 0; i--)
     {
       let e = this.entries[i];
       if (! e.rbm)
@@ -102,14 +102,21 @@ export class MultiBlockMapping
       Object.keys(this.entries[0].bm).forEach(blockid => cb(blockid));
   }
 
-  // Just walks first map
+  // Walk maps in reverse order, careful to not invoke on same geoid
   forEachRev(cb: (geoidid: string, blocks: string[]) => void): void
   {
-    for (let i = 0; i < this.entries.length; i++)
+    let seen = new Set<string>();
+    for (let i = this.entries.length-1; i >= 0; i--)
     {
       let e = this.entries[i];
       if (! e.rbm) e.rbm = reverseBlockMapping(e.bm);
-      Object.keys(e.rbm).forEach(geoid => cb(geoid, e.rbm[geoid]));
+      Object.keys(e.rbm).forEach(geoid => {
+          if (! seen.has(geoid))
+          {
+            seen.add(geoid);
+            cb(geoid, e.rbm[geoid]);
+          }
+        });
     }
   }
 }
